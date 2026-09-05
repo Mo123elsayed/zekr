84de4f87684ddecb2c7117571d5b740b7db3915d
Muhammed Elsayed <133232940+Mo123elsayed@users.noreply.github.com>
2026-09-05 23:50:46 +0300

    Refactor Azkar JSON structure and update details screen
    - Removed the occasional.json file as it is no longer needed.
    - Updated sleep.json to consolidate categories and remove redundant category_id fields.
    - Created new JSON files for each category (adhan, clothes, daily_general, moon, rain, wakeup) with structured azkar data.
    - Updated categories.json to reflect new file paths for each category.
    - Enhanced details_screen.dart by importing app colors and improving button styles for better UI consistency.

--- lib/view/ui/screens/details_screen.dart
+++ lib/view/ui/screens/details_screen.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zekr/core/themes/app_colors.dart';
 import 'package:zekr/core/themes/app_text.dart';
 import 'package:zekr/view/ui/widgets/zikr_card.dart';
 import 'package:zekr/view_model/azkar_cubit/azkar_cubit.dart';
@@ -17,22 +18,26 @@ class _DetailsScreenState extends State<DetailsScreen> {
   int currentIndex = 0;
   late PageController _pageController;
 
-  /// This widget is the root of your application.
-  /// details screen will display the azkar of the selected category. It will receive the category id from the previous screen and use it to fetch the azkar from the repository.
-  /// The azkar will be displayed in a page view, where the user can swipe left or right to see the next or previous azkar. Each page will display the text and transliteration of the azkar.
-  ///
+  /// Initialize the page controller with a viewport fraction of 1.0, which means that each page will take up the entire width of the screen. This allows for a smooth swiping experience between pages.
   @override
   void initState() {
     super.initState();
     _pageController = PageController(viewportFraction: 1.0);
   }
 
+  /// Dispose the page controller when the widget is disposed to free up resources and prevent memory leaks.
   @override
   void dispose() {
     // TODO: implement dispose
     _pageController.dispose();
     super.dispose();
   }
+
+  /// This widget is the root of your application.
+  /// details screen will display the azkar of the selected category. It will receive the category id from the previous screen and use it to fetch the azkar from the repository.
+  /// The azkar will be displayed in a page view, where the user can swipe left or right to see the next or previous azkar. Each page will display the text and transliteration of the azkar.
+  /// The app bar will display the category name and a back button to return to the previous screen. The bottom navigation bar will have a previous button to go back to the previous azkar.
+  ///
 
   @override
   Widget build(BuildContext context) {
@@ -83,14 +88,12 @@ class _DetailsScreenState extends State<DetailsScreen> {
         bottomNavigationBar: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 10,
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: AppColors.secondaryGreen, width: 2),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                backgroundColor: Colors.green[100],
-                side: const BorderSide(color: Colors.green, width: 2),
               ),
               onPressed: () {
                 // Handle previous button press
